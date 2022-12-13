469fa5df444db86eb463c4deff6ae0302a5aeaf3
victorzhou <victorzhoux@163.com>
2022-12-13 09:18:22 +0800

    a domain todo

--- domain/bigModel.go
+++ domain/bigModel.go
@@ -1,6 +1,6 @@
 package domain
 
-type UserWithBigModel struct {
+type UserWithBigModel struct { // TODO: important! UseWithBigModel can not be domain model, domain model must essentail entity, and combine interface in BigModelRcordService in app layer
 	UserName string `json:"username"`
 	BigModel BigModel
 	CreateAt int64 `json:"create_at"`
