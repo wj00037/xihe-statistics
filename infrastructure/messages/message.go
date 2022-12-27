02da4b30381d0646b0ed65d3ee05f956aa582f64
victorzhou123 <victorzhoux@163.com>
2022-12-27 15:49:08 +0800

    🌈 style(messages):

--- infrastructure/messages/message.go
+++ infrastructure/messages/message.go
@@ -6,17 +6,3 @@ type msgStatistics struct {
 	Info     string `json:"info"`
 	CreateAt int64  `json:"create_at"`
 }
-
-type msgBigModelRecord struct {
-	Type     string `json:"type"`
-	UserName string `json:"username"`
-	BigModel string `json:"bigmodel"`
-	CreateAt int64  `json:"create_at"`
-}
-
-type msgRepoRecord struct {
-	Type     string `json:"type"`
-	UserName string `json:"username"`
-	RepoName string `json:"repo_name"`
-	CreateAt int64  `json:"create_at"`
-}
