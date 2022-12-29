d970d4fcba83bd63012fb879d64b2d0f8c4287fd
victorzhou123 <victorzhoux@163.com>
2022-12-29 14:53:27 +0800

    add primary key for pgsql

--- sql/bigmodel_record.sql
+++ sql/bigmodel_record.sql
@@ -1,4 +1,5 @@
 CREATE TABLE "public"."bigmodel_record" (
+	 id bigserial PRIMARY KEY,
 	 username VARCHAR(255) NOT NULL,
 	 bigmodel VARCHAR(255) NOT NULL,
 	 create_at int8 NOT NULL DEFAULT extract(epoch from now())
--- sql/register_record.sql
+++ sql/register_record.sql
@@ -1,4 +1,5 @@
 CREATE TABLE "public"."register_record" (
+	 id bigserial PRIMARY KEY,
 	 username VARCHAR(255) NOT NULL,
 	 create_at int8 NOT NULL DEFAULT extract(epoch from now())
 );
