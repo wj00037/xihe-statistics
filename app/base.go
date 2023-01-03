1a321a32929a0e2e339e9a2ac60c81df7bcbe944
victorzhou123 <victorzhoux@163.com>
2023-01-03 14:43:56 +0800

    change time zone shanghai -> chongqing

--- app/base.go
+++ app/base.go
@@ -2,7 +2,7 @@ package app
 
 import "time"
 
-var cstSh, _ = time.LoadLocation("Asia/Shanghai")
+var cstSh, _ = time.LoadLocation("Asia/Chongqing")
 
 func getLocalTime() (t string) {
 	return time.Now().In(cstSh).Format("2006-01-02 15:04:05+08:00")
