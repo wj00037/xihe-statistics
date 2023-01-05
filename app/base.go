27b08a616f879c04cf89708f4d02c47b9b8985b1
victorzhou123 <victorzhoux@163.com>
2023-01-05 11:45:58 +0800

    ✨ feat(time): change time format
    1. change return time formate

--- app/base.go
+++ app/base.go
@@ -5,7 +5,7 @@ import "time"
 var cstSh, _ = time.LoadLocation("Asia/Shanghai")
 
 func getLocalTime() (t string) {
-	return time.Now().In(cstSh).Format("2006-01-02 15:04:05+08:00")
+	return time.Now().In(cstSh).Format("2006-01-02T15:04:05+08:00")
 
 }
 
