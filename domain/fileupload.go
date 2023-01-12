4eee8ab4e50a1c159951b83081f1afdd6c659efd
victorzhou123 <victorzhoux@163.com>
2023-01-12 17:55:16 +0800

    🌈 style(all): change file name

--- domain/fileupload.go
+++ domain/fileupload.go
@@ -2,6 +2,6 @@ package domain
 
 type FileUploadRecord struct {
 	UserName   Account `json:"username"`
-	UploadPath string `json:"upload_path"`
-	CreateAt   int64  `json:"create_at"`
+	UploadPath string  `json:"upload_path"`
+	CreateAt   int64   `json:"create_at"`
 }
