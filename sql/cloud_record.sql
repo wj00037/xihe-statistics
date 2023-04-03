54099fe0e70d8147744ac7ca628469cc048f61da
victorzhou123 <victorzhoux@163.com>
2023-04-03 15:29:01 +0800

    fix: remove git clone in download

--- sql/cloud_record.sql
+++ sql/cloud_record.sql
@@ -4,4 +4,3 @@ CREATE TABLE "public"."cloud_record" (
 	 cloud_id VARCHAR(255) NOT NULL,
 	 create_at int8 NOT NULL DEFAULT extract(epoch from now())
 );
-CREATE UNIQUE INDEX idx_unq_cloud_record_index_username ON cloud_record USING btree (username);
