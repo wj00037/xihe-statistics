ae33991946586413cee7bb4f7bc8612531265369
victorzhou123 <victorzhoux@163.com>
2023-07-19 10:09:22 +0800

    fix: code checkout from clouddragon

--- app/utils.go
+++ app/utils.go
@@ -1,18 +1,30 @@
 package app
 
-import "time"
+import (
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
 
 var (
-	cstSh, _   = time.LoadLocation("Asia/Shanghai")
 	timeFormat = "2006-01-02T15:04:05+08:00"
 )
 
+func getTimeLocation() *time.Location {
+	cstSh, err := time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		logrus.Debugf("get time location error: %s", err.Error())
+	}
+
+	return cstSh
+}
+
 func getLocalTime() (t string) {
-	return time.Now().In(cstSh).Format(timeFormat)
+	return time.Now().In(getTimeLocation()).Format(timeFormat)
 }
 
 func toStrTime(t int64) string {
-	return time.Unix(t, 0).In(cstSh).Format(timeFormat)
+	return time.Unix(t, 0).In(getTimeLocation()).Format(timeFormat)
 }
 
 func GetUnixLocalTime() (t int64) {
@@ -20,7 +32,7 @@ func GetUnixLocalTime() (t int64) {
 }
 
 func toTimeStamp(t string) (stamp time.Time, err error) {
-	stamp, err = time.ParseInLocation(timeFormat, t, cstSh)
+	stamp, err = time.ParseInLocation(timeFormat, t, getTimeLocation())
 	if err != nil {
 		return
 	}
